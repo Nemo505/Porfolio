68b69be4093ba2dc0a66aa1f441fae6588054322
nemo <dahliahalesia8@gmail.com>
2023-01-21 01:01:03 +0630

    menu

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
-import { AnchorLink } from "react-anchor-link-smooth-scroll";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery"
+
+const Link = ({ page, selectedPage, setSelectedPage}) => {
+    const lowerCasePage = page.toLowerCase();
+
+    return (
+        <AnchorLink 
+        className={`${selectedPage === lowerCasePage ? "text-yellow-300" : "text-white"}
+        hover:text-yellow-300 transition duration-500
+        `}
+        href={`#${lowerCasePage}`}
+        onClick={() => setSelectedPage(lowerCasePage)}
+        >
+            {page}
+        </AnchorLink>
+    )
+}
 
 const Navbar = ({selectedPage, setSelectedPage}) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
@@ -8,7 +24,37 @@ const Navbar = ({selectedPage, setSelectedPage}) => {
   return (
     <nav className={"z-40 w-full fixed top-0 py-6"}>
         <div className={"flex items-center justify-between mx-auto w-5/6"}>
-            
+            <h4 className="font-playfair text-3xl font-bold">JE</h4>
+            {aboveSmallScreens ? (
+                <div className="flex justify-between gap-16 font-mono">
+                    <Link page="Home"
+                        selectedPage={selectedPage}
+                        setSelectedPage={selectedPage}
+                    />
+                    <Link page="Skills"
+                        selectedPage={selectedPage}
+                        setSelectedPage={selectedPage}
+                    />
+                    <Link page="Projects"
+                        selectedPage={selectedPage}
+                        setSelectedPage={selectedPage}
+                    />
+                    <Link page="Testimonials"
+                        selectedPage={selectedPage}
+                        setSelectedPage={selectedPage}
+                    />
+                    <Link page="Contact"
+                        selectedPage={selectedPage}
+                        setSelectedPage={selectedPage}
+                    />
+                </div>
+            ) : (
+                <button className="rounded-full bg-red-300"
+                    onClick={() => setIsMenuToggled(!isMenuToggled)}
+                >
+                    <img src="../assets/maki.jpg" alt="menu-icon" />
+                </button>
+            )}
         </div>
     </nav>
   )
