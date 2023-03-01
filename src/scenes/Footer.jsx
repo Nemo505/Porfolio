05e909e2f494514bd7049d76d6f983c51c2372a8
nemo <dahliahalesia8@gmail.com>
2023-03-01 14:18:28 +0630

    footer re

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -5,11 +5,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-fuchsia-900 mx-auto">
-        <div className="w-5/6 mt-9 py-5">
-            <div className="md:ml-20 ">
+        <div className="mt-6 py-3">
+            <div className="md:flex justify-center text-center md:justify-between md:ml-20 ">
                 <SocialMediaIcons/>
             </div>
-            <div className="md:flex justify-center text-center md:justify-between md:ml-20 ">
+            <div className="md:flex justify-center text-center md:justify-between md:mx-20 ">
                 <p className="font-semibold text-2xl text-fuchsia-300 font-playfait">Shwe Chyu Naing</p>
                 <p>30223 CHYU. All Rights Reserved.</p>
             </div>
