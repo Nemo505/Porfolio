254a4edad71de6ad8803cff0b8e3fd3cae962505
nemo <dahliahalesia8@gmail.com>
2023-03-03 01:20:05 +0630

    projects

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = ({}) => {
         <div className='md:flex gap-16 md:items-center'>
             <div className='basic-2/5 z-10 '>
                 <img src={bubble} 
-                    className='max-w-[300px] md:max-w-[300px] mt-32 md:mt-0 animate-bounce delay-700 duration-10' 
+                    className='max-w-[300px] md:max-w-[300px] mx-auto mt-32 md:mt-0 animate-bounce delay-700 duration-10' 
                     style={{opacity: '0.2'}} alt='' />
             </div>
             <div className='md:order-2 z-30 basis-3/5 md:m-12 md:mt-16'>
@@ -46,8 +46,10 @@ const Projects = ({}) => {
                                     <a href="#">
                                         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Admin Dashboard</h5>
                                     </a>
-                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Writing with react for the products that have chart for sale as well</p>
-                                    <a href="#" className="inline-flex items-center text-fuchsia-700 hover:underline">
+                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                                    Sidebar, Widgets, Charts (Rechart), Data table, Single Page, Dark Mode with React
+                                    </p>
+                                    <a href="https://github.com/Nemo505/admin_react.git" className="inline-flex items-center text-fuchsia-700 hover:underline">
                                         More Details in GitHub
                                         <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                     </a>
@@ -56,10 +58,10 @@ const Projects = ({}) => {
                             <div className="row-start-2 row-span-3  col-span-2">
                                 <div className="max-w-sm p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow hover:animate-pulse">
                                     <a href="#">
-                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Admin Dashboard</h5>
+                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Social Media Page</h5>
                                     </a>
                                     <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Writing with react for the products that have chart for sale as well</p>
-                                    <a href="#" className="inline-flex items-center text-fuchsia-700 hover:underline">
+                                    <a href="https://github.com/Nemo505/React_Social.git" className="inline-flex items-center text-fuchsia-700 hover:underline">
                                         More Details in GitHub
                                         <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                     </a>
@@ -68,10 +70,10 @@ const Projects = ({}) => {
                             <div className="row-span-3  col-span-2">
                                 <div className="max-w-sm p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow hover:animate-pulse">
                                     <a href="#">
-                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Admin Dashboard</h5>
+                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Single Page</h5>
                                     </a>
-                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Writing with react for the products that have chart for sale as well</p>
-                                    <a href="#" className="inline-flex items-center text-fuchsia-700 hover:underline">
+                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400"></p>
+                                    <a href="https://github.com/Nemo505/Laravel-React.git" className="inline-flex items-center text-fuchsia-700 hover:underline">
                                         More Details in GitHub
                                         <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                     </a>
@@ -80,9 +82,11 @@ const Projects = ({}) => {
                             <div className="row-span-3  col-span-2">
                                 <div className="max-w-sm p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow hover:animate-pulse">
                                     <a href="#">
-                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Admin Dashboard</h5>
+                                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Electronic Devices</h5>
                                     </a>
-                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Writing with react for the products that have chart for sale as well</p>
+                                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                                        
+                                    </p>
                                     <a href="#" className="inline-flex items-center text-fuchsia-700 hover:underline">
                                         More Details in GitHub
                                         <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
@@ -94,44 +98,48 @@ const Projects = ({}) => {
                         <div>
                             <div className="max-w-sm mt-8 p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow">
                                 <a href="#">
-                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Admin DashBoard</h5>
                                 </a>
-                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                                <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-                                    See our guideline
+                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                                Sidebar, Widgets, Charts (Rechart), Data table, Single Page, Dark Mode with React
+                                </p>
+                                <a href="https://github.com/Nemo505/admin_react.git" className="inline-flex items-center text-blue-600 hover:underline">
+                                    More Details
                                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                 </a>
                             </div>
 
                             <div className="max-w-sm mt-8 p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow">
                                 <a href="#">
-                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Social Media Page</h5>
                                 </a>
-                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                                <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-                                    See our guideline
+                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                                    
+                                </p>
+                                <a href="https://github.com/Nemo505/React_Social.git" className="inline-flex items-center text-blue-600 hover:underline">
+                                    More Details
                                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                 </a>
                             </div>
 
                             <div className="max-w-sm mt-8 p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow">
                                 <a href="#">
-                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Single Page</h5>
                                 </a>
-                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
-                                <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-                                    See our guideline
+                                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Laravel + React</p>
+                                <a href="https://github.com/Nemo505/Laravel-React.git" className="inline-flex items-center text-blue-600 hover:underline">
+                                    More Details
                                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                 </a>
                             </div>
 
                             <div className="max-w-sm mt-8 p-6 border border-l-4 border-l-violet-600 border-gray-600 rounded-tr-3xl bg-zinc-900 bg-opacity-70 rounded-bl-3xl shadow">
                                 <a href="#">
-                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Electronic Devices Shop</h5>
                                 </a>
                                 <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
                                 <a href="#" className="inline-flex items-center text-blue-600 hover:underline">
-                                    See our guideline
+                                    More Details
                                     <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
                                 </a>
                             </div>
